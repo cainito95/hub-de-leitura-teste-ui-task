9025824ab2f35e7509444318763df350529162c3
cainito95 <yuri.menezes1995@gmail.com>
2026-09-22 17:13:58 -0300

    Atividade E2E realizada EBAC

--- cypress/e2e/end-to-end.cy.js
+++ cypress/e2e/end-to-end.cy.js
@@ -1,28 +1,23 @@
+import { faker } from '@faker-js/faker';
+
 describe('Testes End To End do fluxo de cadastro e login', () => {
 
-    /* 
-    Testes End To End ou Testes de ponta a ponta, ligam uma série de funcionalidades de um sistema,
-    simulando o comportamento do usuário final. Esses testes verificam se diferentes partes do sistema
-    funcionam corretamente quando integradas, garantindo que o fluxo completo de uma funcionalidade
-    funcione como esperado.
-    Aqui iremos criar um teste end to end que cobre o fluxo de cadastro e login de um usuário em um sistema web.
-    Em apenas um teste, ou seja, em um único "it", iremos:
-    1. Acessar a página de cadastro.
-    2. Preencher o formulário de cadastro com dados válidos.
-    3. Submeter o formulário e verificar se o cadastro foi bem-sucedido.
-    4. Acessar a página de login.
-    5. Preencher o formulário de login com as credenciais do usuário recém-cadastrado.
-    6. Submeter o formulário de login e verificar se o login foi bem-sucedido.
-
-    Use as boas práticas aprendidas até agora para estruturar o teste.
-    */
-
     beforeEach(() => {
-        // Configurações iniciais, se necessário
+        cy.visit('register.html')
     });
 
-
     it('Deve fazer o cadastro e validar o login com o usuário cadastrado', () => {
-        // Criar todo o fluxo aqui dentro deste único "it"
+        let name = faker.person.fullName()
+        let email = faker.internet.email()
+        let phone = faker.phone.number()
+        cy.get('#name').type(name)
+        cy.get('#email').type(email)
+        cy.get('#phone').type(phone)
+        cy.get('#password').type('teste123')
+        cy.get('#confirm-password').type('teste123')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        cy.get('#user-name').should('contain', name)
+        cy.loginUsuarioCadastrado(email, name)
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,10 +24,10 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add('login', (email, senha) => { 
-    cy.get('#email').type(email, {log: false})
-    cy.get('#password').type(senha, {log: false})
+Cypress.Commands.add('loginUsuarioCadastrado', (email, name) => {
+    cy.visit('login.html')
+    cy.get('#email').type(email)
+    cy.get('#password').type('teste123')
     cy.get('#login-btn').click()
-    cy.url().should('include', 'dashboard')
- })
-
+    cy.get('#user-name').should('contain', name)
+})
